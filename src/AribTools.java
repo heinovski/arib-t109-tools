2dcb62d53c477603c37673c66ca54595c7a4cffe
Julian Heinovski <julian93@mail.upb.de>
2015-12-16 17:08:01 +0100

    Add optional parameters and multiple calculation ways

--- src/AribTools.java
+++ src/AribTools.java
@@ -3,6 +3,7 @@
  * Tools for ARIB T109 by Julian Heinovski (mail@julian-heinovski.de). 2015.
  * \n\nThese tools provide some functionality which will come in handy while working with the ARIB T109 communication standard for intelligent transport systems.\n
  */
+import java.text.ParseException;
 import java.util.Random;
 
 /**
@@ -24,13 +25,14 @@ public class AribTools {
 	 * 
 	 * @param baseStationCount
 	 */
-	private static void generateRvcPeriods(int baseStationCount) {
+	private static void generateRvcPeriods(int baseStationCount, boolean random, boolean fairness) {
 		System.out.println("\nCalculating Roadside-to-Vehicle period information details for " + baseStationCount
-				+ " base station(s) in the system.");
+				+ " base station(s) in the system using" + (random ? " random" : "") + (fairness ? " fairness" : "") + ".");
 
 		int[] rvcPeriodDurations = new int[_PeriodCount];
 		int[][] transmissionPeriodBegins = new int[baseStationCount][_PeriodCount];
 		int[][] transmissionPeriodDurations = new int[baseStationCount][_PeriodCount];
+		int[] leftTransmissionTimes = new int[_PeriodCount];
 
 		sb.append("\nResults (in us):\n\n");
 		sb.append("Roadside-to-Vehicle period durations:\n\n\"");
@@ -43,15 +45,34 @@ public class AribTools {
 				sb.append(",");
 			}
 
-			for (int j = 0; j < baseStationCount; j++) {
-				// calculate transmission period begins
-				// the transmission period begin is relative to the
-				// Roadside-to-Vehicle period begin. Hence, it's value has to be
-				// between 0 (inclusive) and the Roadside-to-Vehicle period
-				// duration (inclusive).
+			// the transmission period begin is relative to the
+			// Roadside-to-Vehicle period begin. Hence, it's value has to be
+			// between 0 (inclusive) and the Roadside-to-Vehicle period
+			// duration (inclusive).
+			int nextTransmissionPeriodBegin = 0;
+			int currentTransmissionPeriodDuration = 0;
+			int fairTimeSlice = currentPeriodDuration / baseStationCount;
+			int temp = currentPeriodDuration;
 
-				// calculate transmission period durations
+			for (int j = 0; j < baseStationCount; j++) {
+				transmissionPeriodBegins[j][i] = nextTransmissionPeriodBegin;
+
+				if (random) {
+					if (fairness) {
+						currentTransmissionPeriodDuration = r.nextInt(temp + 1);
+					} else {
+						currentTransmissionPeriodDuration = r.nextInt(fairTimeSlice + 1);
+					}
+				} else {
+					currentTransmissionPeriodDuration = fairTimeSlice;
+				}
+				temp -= currentTransmissionPeriodDuration;
+
+				transmissionPeriodDurations[j][i] = currentTransmissionPeriodDuration;
+
+				nextTransmissionPeriodBegin += currentTransmissionPeriodDuration;
 			}
+			leftTransmissionTimes[i] = temp;
 		}
 		sb.append("\"\n");
 
@@ -78,6 +99,14 @@ public class AribTools {
 			}
 			sb.append("\"\n");
 		}
+		sb.append("\nLeft transmission times:\n\n\"");
+		for (int i = 0; i < _PeriodCount; i++) {
+			sb.append(leftTransmissionTimes[i]);
+			if (i + 1 != _PeriodCount) {
+				sb.append(",");
+			}
+		}
+		sb.append("\"\n");
 
 		System.out.print(sb.toString());
 	}
@@ -113,11 +142,35 @@ public class AribTools {
 		if (args.length > 0) {
 			switch (args[0]) {
 			case "periods":
-				generateRvcPeriods(Integer.parseInt(args[1]));
+				int baseStationCount = 0;
+				boolean random = false;
+				boolean fairness = true;
+				if (args.length > 1) {
+					try {
+						baseStationCount = Integer.parseInt(args[1]);
+					} catch (NumberFormatException nfe) {
+						System.out.println("\nInvalid option " + args[1] + ".\nUse --help to display help.");
+					}
+					if (args.length > 2) {
+						random = Boolean.parseBoolean(args[2]);
+						if (args.length > 3) {
+							fairness = Boolean.parseBoolean(args[3]);
+						}
+					}
+				} else {
+					baseStationCount = r.nextInt();
+				}
+				generateRvcPeriods(baseStationCount, random, fairness);
 				break;
 			case "info":
 				printInfo();
 				break;
+			// case "help":
+			// if (args.length > 1) {
+			//
+			// } else {
+			// printHelp();
+			// }
 			case "--help":
 				printHelp();
 				break;
